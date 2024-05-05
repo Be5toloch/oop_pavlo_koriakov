291bd928e19a3089f44615c5ab1cedacc078f332
Be5toloch <pasha.koyakov@gmail.com>
2024-05-05 18:26:21 +0200

    finish 4 task

--- task4.cpp
+++ task4.cpp
@@ -2,44 +2,26 @@
 
 using namespace std;
 
+const int COUNT_OF_VALUES = 4;
+
 class Ipv4Address{
-    int len=4;
+    int len = COUNT_OF_VALUES;
 
 public:
     char *arr;
 
     Ipv4Address():
     arr(new char [len])
+    {
 
     }
 
-    void setter1(char _val1){
-        arr[0]=_val1;
+    void setterValueToArray(char _val, int indexOfArray) {
+        arr[indexOfArray] = _val;
     }
 
-    void setter2(char _val2){
-        arr[1]=_val2;
-    }
-
-    void setter3(char _val3){
-        arr[2]=_val3;
-    }
-
-    void setter4(char _val4){
-        arr[3]=_val4;
-    }
-
-    char getter1(){
-        return arr[0];
-    }
-    char getter2(){
-        return arr[1];
-    }
-    char getter3(){
-        return arr[2];
-    }
-    char getter4(){
-        return arr[3];
+    char getterValueFromArray(int indexOfArray) {
+        return arr[indexOfArray];
     }
 
 };
@@ -48,11 +30,6 @@ public:
 class Request : public Ipv4Address{
     Ipv4Address origin;
 
-    char a = arr[0];
-    char b = arr[1];
-    char c = arr[2];
-    char d = arr[3];
-
 public:
     Request():
     Ipv4Address()
@@ -60,39 +37,40 @@ public:
         
     }
 
-
-
     void log(){
-        cout<<"Request of unknown type from "<<a<<"."<<b<<"."<<c<<"."<<d;
+        cout<<"Request of unknown type from "<<static_cast<int>(arr[0])<<"."<<static_cast<int>(arr[1])<<"."<<static_cast<int>(arr[2])<<"."<<static_cast<int>(arr[3]);
     }
 };
 
 
 int main(){
 
-char val1, val2, val3, val4;
 
-cout<<"Enter the value for a: "<<endl;
-cin>>val1;
-cout<<"Enter the value for b: "<<endl;
-cin>>val2;
-cout<<"Enter the value for c: "<<endl;
-cin>>val3;
-cout<<"Enter the value for d: "<<endl;
-cin>>val4;
+char values[COUNT_OF_VALUES];
 
-Request ad;
+const char* labels[COUNT_OF_VALUES] = {"a", "b", "c", "d"};
 
-ad.setter1(val1);
-ad.setter2(val2);
-ad.setter3(val3);
-ad.setter4(val4);
-/*cout<<ad.getter1()<<endl;
-cout<<ad.getter2()<<endl;
-cout<<ad.getter3()<<endl;
-cout<<ad.getter4()<<endl;*/
-ad.log();
 
+for (int i = 0; i < COUNT_OF_VALUES; i++)
+{
+    cout<<"Enter the value for "<<labels[i]<<": "<<endl;
+    cin>>values[i];
+}
+
+Request request;
+
+for (int i = 0; i < COUNT_OF_VALUES; i++)
+{
+    request.setterValueToArray(values[i], i);
+}
+
+for (int i = 0; i < 4; ++i)
+{
+    
+}
+
+
+request.log();
 
 return 0;
 }
